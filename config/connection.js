48b6198177acf52d7f704de6fc27be4759f2db77
Sunjang1009 <sunjang1009@gmail.com>
2023-04-08 16:59:09 -0400

    competed connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,23 @@
 require("dotenv").config();
 //connecting to mongoose
 const mongoose = require("mongoose");
+
 //getting MONGO_DB_URI
 const connectionString = process.env.MONGO_DB_URI;
 mongoose.connect(connectionString);
+
+moongoose.connection.on("connected", ()=>{
+    console.log(`${new Date().toLocaleDateString()}--- Mongo connected --- ☺️ ☺️ ☺️`);
+});
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("disconnected from Mongoose");
+});
+
+mongoose.connection.on("error", (error)=>{
+    console.log("MongoDB connection error", (error));
+});
+
+//exporting mongoose
+module.exports = mongoose;
 
