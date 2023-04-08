ccb89ec5a185b4e735722e2ee7063972b68feea6
Sunjang1009 <sunjang1009@gmail.com>
2023-04-08 16:24:41 -0400

    connected to PORT

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:false}));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+
+app.get('/', (req,res)=>{
+    res.send("Hello world!")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Now listening on port ${PORT} Ω🔌 🔌Ω`);
+});
