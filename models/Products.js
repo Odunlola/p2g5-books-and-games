6074730ba1d5bacdc3648cd374d8c683e20e96a9
Sunjang1009 <sunjang1009@gmail.com>
2023-04-08 17:07:42 -0400

    completed basic set up for models

--- models/Products.js
+++ models/Products.js
@@ -1,1 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require("../config/connection");
+
+//pull schema and model from mongoose
+const productSchema = new mongoose.Schema(
+    {
+        name:String,
+        author:String,
+        price:Number,
+        image:String,
+        productType:String
+    },{
+        timestamps:true
+    }
+);
+
+const Products = mongoose.model("Products", productSchema);
+module.exports = Products;
