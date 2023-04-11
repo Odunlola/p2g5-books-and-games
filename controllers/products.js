d2511753530e0b185e97a58f37ed4e4b2dba2708
uriapete <uriarteangelo@gmail.com>
2023-04-10 17:30:41 -0700

    created basic show route

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const {Products} = require("../models");
 // index route
 router.get("/",async(req,res,next)=>{
     try {
-        res.send(`Working! You are on the products' index!`);
+        await res.send(`Working! You are on the products' index!`);
     } catch (error) {
         console.log(error);
         res.send(error);
@@ -18,7 +18,17 @@ router.get("/",async(req,res,next)=>{
 // new route
 router.get("/new",async(req,res,next)=>{
     try {
-        res.send(`Working! You are on the products' new page!`);
+        await res.send(`Working! You are on the products' new page!`);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+// show route
+router.get("/:id",async(req,res,next)=>{
+    try {
+        await res.json(await Products.findById(req.params.id));
     } catch (error) {
         console.log(error);
         res.send(error);
@@ -29,7 +39,7 @@ router.get("/new",async(req,res,next)=>{
 router.post("/",async(req,res,next)=>{
     try {
         const newProd = await Products.create(req.body);
-        res.json(newProd);
+        await res.json(newProd);
     } catch (error) {
         console.log(error);
         res.send(error);
