b61f0718235002db69f6c1d8801d46f887a4180d
uriapete <uriarteangelo@gmail.com>
2023-04-15 07:24:04 -0700

    initted api home

---
+++ controllers/api/api.js
@@ -0,0 +1,16 @@
+// setting up express as a router
+const express = require("express");
+const router = express.Router();
+
+// index api
+router.get("/", async (req, res, next) => {
+    try {
+        const products = await Products.find({});
+        res.json({products,status:res.statusCode});
+    } catch (error) {
+        console.log(error);
+        res.json({status:res.statusCode});
+    }
+});
+
+module.exports=router;
