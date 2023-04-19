43aeff6427b441c90613c5a47fd49e4cf4763e64
uriapete <uriarteangelo@gmail.com>
2023-04-19 16:33:40 -0700

    add logic for error in login match + formatting

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const {Users} = require("../models");
+const { Users } = require("../models");
 const bcrypt = require("bcrypt");
 
-router.get("/login",(req,res,next)=>{
+router.get("/login", (req, res, next) => {
     try {
-        res.render("users/login");
+        let error;
+        if (req.query.error==true){
+            error = "Username or password didn't match";
+        }
+        res.render("users/login",{error});
     } catch (error) {
         console.log(error);
         res.send(error);
     }
 })
 
-router.get("/signup",(req,res,next)=>{
+router.get("/signup", (req, res, next) => {
     try {
         res.render("users/signup");
     } catch (error) {
@@ -21,20 +25,20 @@ router.get("/signup",(req,res,next)=>{
     }
 })
 
-router.post("/login",async(req,res,next)=>{
+router.post("/login", async (req, res, next) => {
     try {
         let user;
-        const userExists=await Users.exists({email:req.body.email});
-        if(!userExists){
+        const userExists = await Users.exists({ email: req.body.email });
+        if (!userExists) {
             res.redirect("/login?error=true");
             return 0;
         }
-        user = await Users.findOne({email:req.body.email});
-        const match = await bcrypt.compare(req.body.password,user.password);
-        if(match){
-            req.session.currentUser={
-                id:user._id,
-                username:user.username,
+        user = await Users.findOne({ email: req.body.email });
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (match) {
+            req.session.currentUser = {
+                id: user._id,
+                username: user.username,
             }
             res.redirect("/products");
         } else {
@@ -46,13 +50,13 @@ router.post("/login",async(req,res,next)=>{
     }
 })
 
-router.post("/signup",async(req,res,next)=>{
+router.post("/signup", async (req, res, next) => {
     try {
         const newUser = req.body;
-        const rounds=parseInt(process.env.SALT_ROUNDS);
+        const rounds = parseInt(process.env.SALT_ROUNDS);
         const salt = await bcrypt.genSalt(rounds);
-        const hash = await bcrypt.hash(newUser.password,salt);
-        newUser.password=hash
+        const hash = await bcrypt.hash(newUser.password, salt);
+        newUser.password = hash
         await Users.create(newUser);
         res.redirect("/login");
     } catch (error) {
@@ -61,9 +65,9 @@ router.post("/signup",async(req,res,next)=>{
     }
 })
 
-router.get("/logout",async(req,res,next)=>{
+router.get("/logout", async (req, res, next) => {
     req.session.destroy();
     res.redirect("/login")
 })
 
-module.exports=router
+module.exports = router
