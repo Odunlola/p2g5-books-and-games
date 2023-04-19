2c3ca78423059a18ddb4371afe442565e64d3a9b
uriapete <uriarteangelo@gmail.com>
2023-04-18 18:48:15 -0700

    set up signup post route

--- controllers/users.js
+++ controllers/users.js
@@ -21,4 +21,19 @@ router.get("/signup",(req,res,next)=>{
     }
 })
 
+router.post("/signup",async(req,res,next)=>{
+    try {
+        const newUser = req.body;
+        const rounds=parseInt(process.env.SALT_ROUNDS);
+        const salt = await bcrypt.genSalt(rounds);
+        const hash = await bcrypt.hash(newUser.password,salt);
+        newUser.password=hash
+        await Users.create(newUser);
+        res.redirect("/login");
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
 module.exports=router
