7ea9513aebed96fb0c53f743e61dad32ac40a99e
uriapete <uriarteangelo@gmail.com>
2023-04-18 18:32:44 -0700

    create get routes for login and signup

--- controllers/users.js
+++ controllers/users.js
@@ -2,3 +2,23 @@ const express = require("express");
 const router = express.Router();
 const {Users} = require("../models");
 const bcrypt = require("bcrypt");
+
+router.get("/login",(req,res,next)=>{
+    try {
+        res.render("users/login");
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+router.get("/signup",(req,res,next)=>{
+    try {
+        res.render("users/signup");
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+module.exports=router
